1c0ff2402f28396328bd68c65e674d097f9771ed
Landyn <landynmoreno@gmail.com>
2026-07-14 17:18:27 -0500

    Remove the alw claim stub (#556)

--- allways/cli/swap_commands/__init__.py
+++ allways/cli/swap_commands/__init__.py
@@ -1,5 +1,4 @@
 from allways.cli.swap_commands.admin import admin_group
-from allways.cli.swap_commands.claim import claim_command
 from allways.cli.swap_commands.collateral import collateral_group
 from allways.cli.swap_commands.miner_commands import miner_group
 from allways.cli.swap_commands.numeraire import quotes_command
@@ -26,7 +25,6 @@ def register_commands(cli):
     cli.add_command(collateral_group, 'collateral')
     cli.add_command(swap_group, 'swap')
     cli.add_command(view_group, 'view')
-    cli.add_command(claim_command, 'claim')
     cli.add_command(miner_group, 'miner')
     cli.add_command(admin_group, 'admin')
     cli.add_command(status_command, 'status')
